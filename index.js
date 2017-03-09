84e79f8a88e33a7b1e5d003291d592f6955ceea5
e174596549 <ly6819@hotmail.com>
2017-03-09 18:41:35 +0800

    add electron demo by liuyuan

--- index.js
+++ index.js
@@ -313,15 +313,64 @@ $(".js-save-board-btn").on('click', () => {
     for (let i = 0; i < boardArr.length; i++) {
         //	console.log(boardArr[i].parentElement.classList)
         if (model == 0) {
-            saveArr.push(boardArr[i].value)
+            saveArr.push(Number(boardArr[i].value))
         } else {
             for (let j = 0; j < clickClassList.length; j++) {
                 if (boardArr[i].parentElement.classList.contains(clickClassList[j])) {
-                    saveArr.push([j, boardArr[i].value])
+                    saveArr.push([j, Number(boardArr[i].value)])
                 }
             }
         }
     }
     console.log(saveArr)
+    var fs = require('fs');
+    fs.mkdir(`${__dirname}/../../../data`, 0777, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("creat done!");
+        }
+    })
+    _saveJSON(`${__dirname}/../../.././data/test.num`, saveArr)
+    console.log(__dirname)
 })
 //TODO 发送已存信息
+
+const cachedUrl = function(options, callback) {
+    const fs = require('fs')
+    // 先生成对应的文件
+    const path = options.url.split('/').join('-').split(':').join('-')
+    // 先尝试去硬盘中读取这个 url 对应的文件
+    fs.readFile(path, function(err, data) {
+        if (err != null) {
+            // 读取这个文件失败
+            // 读不到的话说明是第一次请求，那么就使用 request
+            request(options, function(error, response, body) {
+                // 下载好了之后，保存到本地文件
+                // TODO, 应该下载成功之后再保存
+                writeToFile(path, body)
+                callback(error, response, body)
+            })
+        } else {
+            log('读取到缓存的页面', path)
+            // 读取到，说明已经下载过了，我们讲直接读取硬盘上的文件
+            const response = {
+                statusCode: 200,
+            }
+            callback(null, response, data)
+        }
+    })
+}
+
+const _saveJSON = function(path, answers) {
+    // 这个函数用来把一个保存了所有对象的数组保存到文件中
+    const fs = require('fs')
+    const s = JSON.stringify(answers, null, 2)
+    fs.writeFile(path, s, function(error) {
+        if (error !== null) {
+            console.log('*** 写入文件错误', error)
+        } else {
+            console.log('--- 保存成功')
+        }
+    })
+}
